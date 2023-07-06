d2de00cc2c870bf28c8ed75e1bad1ac4ace0d545
Andrea Toledo Cadario <andrea.toledo.cadario@mi.unc.edu.ar>
2023-07-05 23:58:28 -0300

    clase 20

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -10,3 +10,18 @@ def curso (self):
 
     documentoDeTexto=f'El curso es umuy bueno y se llama {curso.nombre}, y la camada es {curso.camada}'
     return HttpResponse (documentoDeTexto)
+
+def inicio(request):
+    return render(request, 'AppCoder/inicio.html')
+
+def cursos(request):
+    return render(request, 'AppCoder/cursos.html')
+
+def profesores(request):
+    return render(request, 'AppCoder/profesores.html')
+
+def estudiantes(request):
+    return render(request, 'AppCoder/estudiantes.html')
+
+def entregables(request):
+    return render(request, 'AppCoder/entregables.html')
